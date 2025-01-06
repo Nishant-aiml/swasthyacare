39d51478adeec6a48faccb2fabed6b2d323953e5
Nishant Datta <dattanishant2@gmail.com>
2025-01-06 08:13:38 +0530

    Update build configuration

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': require('path').resolve(__dirname, './src'),
-    };
-    return config;
-  },
   reactStrictMode: true,
+  swcMinify: true,
   typescript: {
     ignoreBuildErrors: true,
   },
